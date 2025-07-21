cd1af0408f002fac01ca5ccd03db9627ce61aca3
yulianto7380 <yuliantorahmawati73@gmail.com>
2025-07-21 11:57:17 +0700

    Belajar Flutter

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BelajarFlutter());
+}
+
+class BelajarFlutter extends StatelessWidget {
+  const BelajarFlutter({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Hello'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Text('Hello Flutter', style: TextStyle(
+            color: Colors.amberAccent,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.blueGrey, 
+          ),),
+        ),
+      ),
+    );
+  }
+}
